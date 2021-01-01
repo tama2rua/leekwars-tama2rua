7ef06e448135c8ab7e10c3204b307cc2200870e5
tama2rua <tamarua.ruahe@gmail.com>
2020-12-31 21:12:02 -1000

    vesrion beta 0.2 - code de base du poireau

--- poireau.js
+++ poireau.js
@@ -1,2 +1,11 @@
-var poireau = tams2rua;
-var canvas = [];
+//On prend le pistolet
+setWeapon(WEAPON_PISTOL); //coûte 1PT
+
+//On récupère l'ennemi le plus proche
+var enemy = getNearestEnemy();
+
+//On avance vers l'ennemi
+moveToward(enemy);
+
+//On essaye de lui tirer dessus !
+useWeapon(enemy);
